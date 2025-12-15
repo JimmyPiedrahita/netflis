1867871c75f5e7b9557f5ca562b14e61eeabd040
Jimmy Piedrahita <jimmy22piedrahita@gmail.com>
2025-12-15 15:24:45 -0500

    Remove the title "Netflis" from the header component for a cleaner UI.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,6 @@ const Header = ({ user, logout }) => {
     <header className="app-header">
       <div className="logo-container">
         <img src="/logo.svg" alt="Logo" style={{ height: '40px', width: 'auto' }} />
-        <h1>Netflis</h1>
       </div>
       {user && (
         <button onClick={logout} className="btn btn-logout">
